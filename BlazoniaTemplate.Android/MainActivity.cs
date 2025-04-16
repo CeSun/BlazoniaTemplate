2f595b8654e084dfc5745a32fe0f9afdea20c40d
CeSun <ce.sun@outlook.com>
2025-04-16 19:04:24 +0800

    添加项目文件。

---
+++ BlazoniaTemplate.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+using Blazonia;
+
+namespace BlazoniaTemplate.Android;
+
+[Activity(
+    Label = "BlazoniaTemplate.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont()
+            .UseAvaloniaBlazorBindings(services => { });
+    }
+}
---
+++ BlazoniaTemplate/App.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+using Blazonia;
+using BlazoniaTemplate.Pages;
+
+namespace BlazoniaTemplate;
+
+public class App : AppBase
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        base.OnFrameworkInitializationCompleted();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+}
+public class AppBase : BlazoniaApplication<MainPage>
+{
+}
